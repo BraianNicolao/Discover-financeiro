04981bb7e7e0dd27d23a66c05a5a5877170500df
Arkaitus <braian.codex@gmail.com>
2021-09-13 15:55:42 -0300

    Adicionado objects e metodos de transactions

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ const Modal = {
     .classList.remove('active');
   }
 }
+
+const transactions = [{
+  id: 1,
+  description: 'Luz',
+  amount: -50000,
+  date: '23/01/2021',
+},
+{
+  id: 2,
+  description: 'Website',
+  amount: 500000,
+  date: '23/01/2021',
+},
+{
+  id: 3,
+  description: 'Internet',
+  amount: -200000,
+  date: '23/01/2021',
+},
+]
+
+const Transaction = {
+  incomes(){
+    //somar as entradas
+  },
+  expenses(){
+    //somar as saídas
+  },
+  total(){
+    //entrada - saídas
+  }
+}
